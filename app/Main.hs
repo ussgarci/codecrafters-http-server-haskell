da824e06b9b0017006fd825a2a18dd84d176b268
sgarcia <stevensantosgarcia@gmail.com>
2025-07-18 12:13:34 -0500

    First stage

--- app/Main.hs
+++ app/Main.hs
@@ -17,22 +17,22 @@ main = do
     BC.putStrLn "Logs from your program will appear here"
 
     -- Uncomment this block to pass first stage
-    -- let host = "127.0.0.1"
-    --     port = "4221"
-    --
-    -- BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
-    --
-    -- -- Get address information for the given host and port
-    -- addrInfo <- getAddrInfo Nothing (Just host) (Just port)
-    --
-    -- serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
-    -- bind serverSocket $ addrAddress $ head addrInfo
-    -- listen serverSocket 5
-    --
-    -- -- Accept connections and handle them forever
-    -- forever $ do
-    --     (clientSocket, clientAddr) <- accept serverSocket
-    --     BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
-    --     -- Handle the clientSocket as needed...
-    --
-    --     close clientSocket
+    let host = "127.0.0.1"
+        port = "4221"
+    
+    BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
+    
+    -- Get address information for the given host and port
+    addrInfo <- getAddrInfo Nothing (Just host) (Just port)
+    
+    serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
+    bind serverSocket $ addrAddress $ head addrInfo
+    listen serverSocket 5
+    
+    -- Accept connections and handle them forever
+    forever $ do
+        (clientSocket, clientAddr) <- accept serverSocket
+        BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
+        -- Handle the clientSocket as needed...
+    
+        close clientSocket
