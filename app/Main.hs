0eeda8ddf2ad04208994b69252bcd38dab2b4428
sgarcia <stevensantosgarcia@gmail.com>
2025-08-07 11:03:06 -0500

    Move parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,71 +2,14 @@
 
 module Main (main) where
 
-import Control.Monad (forever, when)
-import qualified Data.ByteString as B
+import Control.Monad (forever)
 import qualified Data.ByteString.Char8 as BC
-import Data.Char (ord)
-import qualified Data.Map as M
-import Data.Void (Void)
-import Data.Word (Word8)
-import Debug.Trace
 import qualified Network.Socket as NS
 import Network.Socket.ByteString (recv, sendAll)
+import Parser.HttpRequest
 import System.Console.ANSI
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stderr, stdout)
 import qualified Text.Megaparsec as MP
-import qualified Text.Megaparsec.Byte as MPB
-
-data Header = Header
-    { _name :: BC.ByteString
-    , _value :: BC.ByteString
-    }
-    deriving (Show)
-
-data HttpRequest = HttpRequest
-    { _method :: BC.ByteString
-    , _target :: BC.ByteString
-    , _version :: BC.ByteString
-    , _headers :: [Header]
-    -- , _body :: Maybe BC.ByteString
-    }
-    deriving (Show)
-
-type Parser = MP.Parsec Void BC.ByteString
-
-charCodesMap :: M.Map Char Word8
-charCodesMap =
-    M.fromList
-        [ (' ', fromIntegral (ord ' ') :: Word8)
-        , (':', fromIntegral (ord ':') :: Word8)
-        , ('\r', fromIntegral (ord '\r') :: Word8)
-        ]
-
-parseBody :: Parser BC.ByteString
-parseBody = undefined
-
-parseHeader :: Parser Header
-parseHeader = do
-    name <- MP.takeWhile1P (Just "http header name") (/= (charCodesMap M.! ':'))
-    _ <- MPB.char (charCodesMap M.! ':')
-    _ <- MPB.space
-    value <- MP.takeWhile1P (Just "http header value") (/= (charCodesMap M.! '\r'))
-    _ <- MPB.crlf
-    return $ Header name value
-
-parseHttpRequest :: Parser HttpRequest
-parseHttpRequest = do
-    MPB.space
-    -- change to MPB.takeWhile1P (Just "http method") BC8.isLetter_w8
-    -- method <- MPB.string "GET"
-    method <- B.pack <$> MP.many MPB.letterChar
-    MPB.space
-    target <- MP.takeWhile1P (Just "http request target") (/= (charCodesMap M.! ' '))
-    MPB.space
-    version <- MPB.string "HTTP/1.1"
-    _ <- MPB.crlf
-    headers <- MP.manyTill parseHeader MPB.crlf
-    return $ HttpRequest method target version headers
 
 route :: NS.Socket -> HttpRequest -> IO ()
 route socket (HttpRequest{_method = "GET", _target = "/"}) = sendAll socket (BC.pack "HTTP/1.1 200 OK\r\n\r\n")
---
+++ src/Parser/HttpRequest.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser.HttpRequest (HttpRequest (..), Header (..), parseHttpRequest)
+where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import Data.Char (ord)
+import qualified Data.Map as M
+import Data.Void (Void)
+import Data.Word (Word8)
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Byte as MPB
+
+data Header = Header
+    { _name :: BC.ByteString
+    , _value :: BC.ByteString
+    }
+    deriving (Show)
+
+data HttpRequest = HttpRequest
+    { _method :: BC.ByteString
+    , _target :: BC.ByteString
+    , _version :: BC.ByteString
+    , _headers :: [Header]
+    -- , _body :: Maybe BC.ByteString
+    }
+    deriving (Show)
+
+type Parser = MP.Parsec Void BC.ByteString
+
+charCodesMap :: M.Map Char Word8
+charCodesMap =
+    M.fromList
+        [ (' ', fromIntegral (ord ' ') :: Word8)
+        , (':', fromIntegral (ord ':') :: Word8)
+        , ('\r', fromIntegral (ord '\r') :: Word8)
+        ]
+
+parseBody :: Parser BC.ByteString
+parseBody = undefined
+
+parseHeader :: Parser Header
+parseHeader = do
+    name <- MP.takeWhile1P (Just "http header name") (/= (charCodesMap M.! ':'))
+    _ <- MPB.char (charCodesMap M.! ':')
+    _ <- MPB.space
+    value <- MP.takeWhile1P (Just "http header value") (/= (charCodesMap M.! '\r'))
+    _ <- MPB.crlf
+    return $ Header name value
+
+parseHttpRequest :: Parser HttpRequest
+parseHttpRequest = do
+    MPB.space
+    -- change to MPB.takeWhile1P (Just "http method") BC8.isLetter_w8
+    -- method <- MPB.string "GET"
+    method <- B.pack <$> MP.many MPB.letterChar
+    MPB.space
+    target <- MP.takeWhile1P (Just "http request target") (/= (charCodesMap M.! ' '))
+    MPB.space
+    version <- MPB.string "HTTP/1.1"
+    _ <- MPB.crlf
+    headers <- MP.manyTill parseHeader MPB.crlf
+    return $ HttpRequest method target version headers
