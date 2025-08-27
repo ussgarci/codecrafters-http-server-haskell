8ce3467a34ac9dff7b2f3c9b5c744c44831fd11b
sgarcia <stevensantosgarcia@gmail.com>
2025-08-26 20:48:07 -0500

    echo

--- src/Http/Router.hs
+++ src/Http/Router.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Http.Router (route)
 where
 
+import qualified Codec.Compression.GZip as GZip (compress)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ask)
 import Control.Monad.State (MonadState, get, modify, put)
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BCL
 import Http.Types (Config, ConnectionState (..), HeaderName, HeaderValue, HttpRequest (..), HttpResponse (..))
 
 notFoundResponse :: HttpResponse
@@ -36,7 +39,21 @@ handleUserAgent (HttpRequest{_headers = hs}) = pure $
         Nothing -> okResponse [("Content-Type", "text/plain")] ""
 
 handleEcho :: Monad m => HttpRequest -> m HttpResponse
-handleEcho request = undefined
+handleEcho HttpRequest{_headers, _target} = do
+    case BC.stripPrefix "/echo/" _target of
+        Just str -> do
+            let (encodingHeader, compressedBody) = handleEncoding _headers str
+            pure $ okResponse (("Content-Type", "text/plain") : encodingHeader) compressedBody
+        Nothing -> pure notFoundResponse
+
+handleEncoding :: [(HeaderName, HeaderValue)] -> BC.ByteString -> ([(HeaderName, HeaderValue)], BC.ByteString)
+handleEncoding headers body =
+    case lookup "Accept-Encoding" headers of
+        Just encodings
+            | "gzip" `BC.isInfixOf` encodings ->
+                let compressed = BCL.toStrict . GZip.compress . BCL.fromStrict $ body
+                 in ([("Content-Encoding", "gzip")], compressed)
+        _ -> ([], body)
 
 handleFiles :: (MonadReader Config m, MonadIO m) => HttpRequest -> m HttpResponse
 handleFiles request = do
